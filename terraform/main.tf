1bc3e1c66ebd8cf1a1589f660517e2b9d6ffb379
Alec LIM <alec.lim@epita.fr>
2023-11-21 15:59:58 +0300

    [FIX] terraform syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "flask_api" {
-  ami           = "d-c3671f900d"  # Remplacez par l'ID de l'AMI souhaité
-  instance_type = "t2.micro" # Ou choisissez une instance appropriée
+  ami           = "ami-id"  # Remplacez par l'ID de l'AMI souhaité
+  instance_type = "t2.micro"
 
   tags = {
     Name = "FlaskAPIInstance"
-  
+  }
+
   provisioner "file" {
     source      = "./install.sh"
     destination = "/tmp/install.sh"
